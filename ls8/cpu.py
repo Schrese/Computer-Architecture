22657482feb1eceab89ee546e2a1f19d27d05b50
Schrese <schreseholloway@gmail.com>
2020-07-20 19:07:59 -0400

    finished

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,6 +3,10 @@
 import sys
 
 # 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
 
 class CPU:
     """Main CPU class."""
@@ -11,7 +15,7 @@ class CPU:
         """Construct a new CPU."""
         # pass
         self.properties = [0] * 256
-        self.gpr = [0] * 8
+        self.reg = [0] * 8 # reg = general purpose registers
         self.pc = 0
         # self.ir --> possibly to be used later
         # self.mar --> possibly to be used later
@@ -20,7 +24,7 @@ class CPU:
 
     def ram_read(self, MAR):
         answer = self.properties[MAR]
-        print(answer)
+        # print(answer)
         return answer
 
     def ram_write(self, MAR, MDR):
@@ -81,19 +85,35 @@ class CPU:
     def run(self):
         """Run the CPU."""
         # pass
-        IR = self.ram_read(self.pc)
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
+        cont = True
 
-        if IR == 0b10000010:
-            print('hello')
-            self.pc += 2
-        elif IR == 0b01000111:
-            print('interesting')
+        while cont:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == 0b10000010: #LDI
+                print('hello')
+                self.LDI_Handler(operand_a, operand_b)
+                self.pc += 2
+            elif IR == 0b01000111: # PRN
+                print('interesting')
+                self.PRN_Handler(operand_a)
+                self.pc += 1
+            elif IR == HLT:
+                print('ohhhhh myyyyyyyy')
+                cont = False
+                # sys.exit(1)
+                # return
+            # else:
+            #     print('what do?')
             self.pc += 1
-        elif IR == 0b00000001:
-            print('ohhhhh myyyyyyyy')
-            return
-        else:
-            print('what do?')
 
+    # Set the value of a register to an integer.
+    def LDI_Handler(self, key, value):
+        self.reg[key] = value
+        print(value, "LDI_Handler")
+        
+    # Print numeric value stored in the given register.
+    def PRN_Handler(self, value):
+        print(self.reg[value], 'PRN_Handler')
