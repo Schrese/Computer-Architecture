04189c3a667ed00ec239c69f32306405d85cd3d4
Schrese <schreseholloway@gmail.com>
2020-07-23 17:01:34 -0400

    first attempt at Call

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,7 +6,11 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
-
+MULT = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
 
 class CPU:
     """Main CPU class."""
@@ -16,11 +20,10 @@ class CPU:
         # pass
         self.properties = [0] * 256
         self.reg = [0] * 8 # reg = general purpose registers
-        self.pc = 0
-        # self.ir --> possibly to be used later
-        # self.mar --> possibly to be used later
-        # self.mdr --> possibly to be used later
-        # self.fl --> possibly to be used later
+        self.pc = 0 # ---> program counter
+        self.sp = 0xF4 # --> Stack Pointer
+        self.ir = 0
+
 
     def ram_read(self, MAR):
         answer = self.properties[MAR]
@@ -29,6 +32,7 @@ class CPU:
 
     def ram_write(self, MAR, MDR):
         self.properties[MAR] = MDR
+        return
         
 
     def load(self):
@@ -108,7 +112,7 @@ class CPU:
     def run(self):
         """Run the CPU."""
         # pass
-        self.trace()
+        # self.trace()
         cont = True
 
         while cont:
@@ -116,33 +120,57 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if IR == 0b10000010: #LDI
+            if IR == LDI: #LDI
                 # print('hello')
                 self.LDI_Handler(operand_a, operand_b)
-                self.pc += 2
-            elif IR == 0b01000111: # PRN
+                self.pc += 3
+            elif IR == PRN: # PRN
                 # print('interesting')
                 self.PRN_Handler(operand_a)
-                self.pc += 1
-            elif IR == 0b10100000: # ADD
-                self.alu(operand_a, operand_b)
-            elif IR == 0b10100010: # MULT
-                self.alu("MULT", operand_a, operand_b)
+                self.pc += 2
+            # elif IR == 0b10100000: # ADD
+            #     self.alu("ADD", operand_a, operand_b)
+            elif IR == MULT: # MULT
+                self.alu("MULT", self.pc + 1, self.pc + 2)
                 # self.MULT_Handler(operand_a, operand_b)
-                # self.pc += 2
+                self.pc += 2
+            elif IR == PUSH:
+                # self.sp -=1 # Moves the pointer down/decrements it
+                registry_address = self.ram_read(self.pc + 1)
+                self.PUSH_Handler(self.reg[registry_address])
+                # self.ram_write(self.sp, self.reg[registry_address])
+                self.pc += 2
+            elif IR == POP:
+                value = self.POP_Handler()
+                registry_address = self.ram_read(self.pc + 1)
+                self.reg[registry_address] = value
+                self.pc += 2
             elif IR == HLT:
                 # print('ohhhhh myyyyyyyy')
                 cont = False
                 # sys.exit(1)
                 # return
-            # else:
-            #     print('what do?')
-            self.pc += 1
+            elif IR == CALL:
+                # pass
+                self.reg[self.pc] -= 1
+                self.PUSH_Handler(self.pc + 2)
+
+                reg_num = self.properties[self.pc + 1]
+                subroutine_addr = self.reg[reg_num]
+
+                self.pc = subroutine_addr
+
+            elif IR == RET:
+                # pass
+                return_addr = self.POP_Handler(self.pc + 1)
+                self.reg[self.sp] += 1
+            else:
+                print('what do?')
 
     # Set the value of a register to an integer.
     def LDI_Handler(self, key, value):
         self.reg[key] = value
-        print(value, 'coming from here')
+        # print(value, 'coming from here')
         
     # Print numeric value stored in the given register.
     def PRN_Handler(self, value):
@@ -153,3 +181,22 @@ class CPU:
     #     self.reg[val1] *= self.reg[val2]
     #     print(self.reg[val1], 'total')
     #     # return total
+    def PUSH_Handler(self, v):
+        self.sp -=1 
+        self.ram_write(self.sp, v)
+        # self.pc += 2
+
+    def POP_Handler(self):
+        if self.sp < 0xF4:
+            value = self.ram_read(self.sp)
+            self.sp += 1
+            return value
+        else:
+            print("Error: stack is empty")
+            cont = False    
+
+    def CALL_Handler(self):
+        pass
+
+    def RET_Handler(self):
+        pass
